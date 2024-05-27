4cfc3a3793e95fb4ea32d589093c44f64d0ef075
meowlet <ngoclink13@gmail.com>
2024-05-28 03:35:02 +0700

    Feature: Make new wraper for fiction-related features

--- src/app/User.ts
+++
@@ -1,6 +0,0 @@
-import Elysia from "elysia";
-import { SignInController } from "../controller/SignInController";
-import { SignUpController } from "../controller/SignUpController";
-
-export const UserApplication = (app: Elysia) =>
-  app.use(SignUpController).use(SignInController);
