d622800de87fcf2e8f34b558ddf5fd881b6bd9fe
meowlet <ngoclink13@gmail.com>
2024-06-01 19:06:01 +0700

    Refactor: Fix the server depth

--- himmel-server/src/app/UserApp.ts
+++
@@ -1,6 +0,0 @@
-import Elysia from "elysia";
-import { SignInController } from "../controller/auth/SignInController";
-import { SignUpController } from "../controller/auth/SignUpController";
-
-export const UserApplication = (app: Elysia) =>
-  app.use(SignUpController).use(SignInController);
--- himmel-server/src/database/MongoDBSetup.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-const mongoDBURI = process.env.MONGODB_URI || "MONGDB_URI";
-
-await mongoose.connect(mongoDBURI);
-
-export default mongoose;
