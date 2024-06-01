d8c72d24fa3a751464d37783a93f78c1c9f3df66
meowlet <ngoclink13@gmail.com>
2024-06-01 14:53:13 +0700

    Chore: Add "AddUserRole.ts" to the wrapper

--- src/app/RoleApp.ts
+++ src/app/RoleApp.ts
@@ -1,4 +1,6 @@
 import Elysia from "elysia";
 import { AddRoleController } from "../controller/perm/AddRoleController";
+import { AddUserRoleController } from "../controller/perm/AddUserRoleController";
 
-export const RoleApplication = (app: Elysia) => app.use(AddRoleController);
+export const RoleApplication = (app: Elysia) =>
+  app.use(AddRoleController).use(AddUserRoleController);
