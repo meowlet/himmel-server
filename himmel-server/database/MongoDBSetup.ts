3f656bba43b3f0a981bc026fed2b58e2bc45aad6
Trương Minh Nhật <fleeforezz@gmail.com>
2024-06-03 23:08:46 +0700

    Update MongoDBSetup.ts

--- himmel-server/database/MongoDBSetup.ts
+++ himmel-server/database/MongoDBSetup.ts
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 const mongoDBURI = process.env.MONGODB_URI || "MONGDB_URI";
 
-await mongoose.connect("mongodb+srv://fleeforezz:u6pkrbV4JM9SOHh@cluster0.ohxanmw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+await mongoose.connect(mongoDBURI);
 
 export default mongoose;
